76da50e27e933c96cec187b1e634e1d524b10f87
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-08 16:01:54 +0100

    relase new project

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// consegna
+
+/*
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*Prima di partire a scrivere codice poniamoci qualche domanda:*
+Come faccio a sapere se un numero è divisibile per un altro?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+*Consigli del giorno:*
+1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+*BONUS 1:*
+Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+*BONUS 2:*
+Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+*/
+
+// variabili
+let htmlTmp = '';
+let cardCont = '';
+let cardNum = 100;
+// stampo risultati all'interno della console
+
+// utilizzo un ciclo for per dichiarare fino a che numero devo prender
+for (let i = 1; i <= 100; i++){
+    // inserisco la prima consizione (per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.)
+    if(i % 3 === 0 && i % 5 === 0){
+        // console.log('FizzBuzz');
+        cardCont = 'FizzBuzz'
+    // inserisco la seconda consizione (per i multipli di 5 stampi “Buzz”.)
+    } else if(i % 5 === 0){
+        // console.log('Buzz');
+        cardCont = 'Buzz'
+    // inserisco la terza consizione (per i multipli di 3 stampi “Fizz”)
+    } else if(i % 3 === 0){
+        // console.log('Fizz');
+        cardCont = 'Buzz'
+    // inserisco l'ultima consizione (per tutto ciò che non rispetta la condizione 1, 2 e 3)
+    } else {
+        console.log(i)
+    } 
+}
+
+// ... aquesto punto mi posso divertire un pò di più, creando un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+
+// creo un elemento div all'interno del mio html
+const divContainer = document.createElement('div');
+// console.log(divContainer);
+
+// inserisco le classi e id all'elemento creato
+divContainer.className = 'd-flex justify-content-between align-content-center flex-wrap';
+
+//creo un clico per la quantità di elementi presenti all'interno del div
+for(let i = 1; i <= cardNum; i++ ){
+    htmlTmp += `<div>${cardCont}</div>`;
+}
+
+// sostituisco il console.log all'interno delle mie condizioni  con una variabile che dichiaro prima(cardCont);
+
+// appendo il risultato allintero
+const htmlApp = document.querySelector('.app');
+
