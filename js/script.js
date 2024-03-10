959f478d768ce2415f8ba429d6d531ee49a2f21c
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-10 18:10:43 +0100

    (js) insert img ansd style html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,64 @@
+/*
+<div class="container">  sectContainer
+  <div class="row">  rowContainer
+    <div class="col"></div>    colContainer
+    <div class="col"></div>    colContainer2
+  </div>
+  <div class="row">    rowContainer2 
+    <div class="col"></div>    colContainer3
+    <div class="col"></div>    colContainer4
+    <div class="col"></div>    colContainer5
+  </div>
+</div>
+*/
+
+// generiamo nuovi elementi da inserire in pagina successivamente 
+const sectContainer = document.createElement('div');
+const rowContainer = document.createElement('div');
+const colContainer = document.createElement('div');
+const colContainer2 = document.createElement('div');
+const rowContainer2 = document.createElement('div');
+const colContainer3 = document.createElement('div');
+const colContainer4= document.createElement('div');
+const colContainer5 = document.createElement('div');
+const img1 = document.createElement('img');
+// generiamo un contenitore delle immagini
+let arrayImg = ['../img/math2_1.jpeg', '../img/math2_2.jpeg', '../img/math2_3.jpeg'];
+// generiamo una scala gerarchica tra gli elementi
+sectContainer.appendChild(rowContainer);
+rowContainer.appendChild(colContainer);
+rowContainer.appendChild(colContainer2);
+// assegnamo le classi desiderate
+sectContainer.className ='container'
+rowContainer.className ='row d-flex justify-content-between'
+colContainer.className ='col fs-1 font-I text-light d-flex justify-content-center align-items-center'
+colContainer2.className ='col d-flex justify-content-center align-items-center' 
+
+console.log(sectContainer);
+// inseriamo un elemento di testo
+colContainer.innerText = 'Fizz buzz is a group word game for children to teach them about division. Players take turns to count incrementally, replacing any number divisible by three with the word "fizz", and any number divisible by five with the word "buzz", and any number divisible by both three and five with the word "fizzbuzz".'
+
+// generazione ciclo for per Img
+for(let s = 0; s < arrayImg.length; s++) {
+  // creiamo una costante nella quale inserire l'icremento degli elementi dell'array
+  const imgElement = arrayImg[s];
+  // generiamo un elementi img
+  let img = document.createElement('img');
+  // assegnamo l'attributo src 
+  img.setAttribute("src", imgElement);
+  // appendiamo il tutto nel div desiderato
+  colContainer2.append(img);
+
+
+}
+
+// scriviamo gli elementi nello html
+ const htmlCont  = document.getElementById('section1')
+ htmlCont.append(sectContainer)
+
+
+
+
 // consegna
 
 /*
@@ -56,7 +117,7 @@ for (let i = 1; i <= 100; i++) {
     // creo un elemento div all'interno del mio html
     const divContainer = document.createElement('div');
     // console.log(divContainer);    // inserisco le classi e id all'elemento creato
-    divContainer.className = 'cardapp d-flex justify-content-center align-items-center m-1 text-light';    // aggiunto un listener per inserire le classi con il mouseover
+    divContainer.className = 'cardapp font-I d-flex justify-content-center align-items-center m-1 text-light';    // aggiunto un listener per inserire le classi con il mouseover
     divContainer.addEventListener('mouseover', () => {
         divContainer.classList.toggle(cardScale);
         divContainer.classList.toggle(bgCard);
@@ -72,52 +133,3 @@ for (let i = 1; i <= 100; i++) {
 
 
 }
-/*
-<div class="container">  sectContainer
-  <div class="row">  rowContainer
-    <div class="col"></div>    colContainer
-    <div class="col"></div>    colContainer2
-  </div>
-  <div class="row">    rowContainer2 
-    <div class="col"></div>    colContainer3
-    <div class="col"></div>    colContainer4
-    <div class="col"></div>    colContainer5
-  </div>
-</div>
-*/
-
-
-const sectContainer = document.createElement('div');
-const rowContainer = document.createElement('div');
-const colContainer = document.createElement('div');
-const colContainer2 = document.createElement('div');
-const rowContainer2 = document.createElement('div');
-const colContainer3 = document.createElement('div');
-const colContainer4= document.createElement('div');
-const colContainer5 = document.createElement('div');
-
-sectContainer.appendChild(rowContainer);
-rowContainer.appendChild(colContainer);
-rowContainer.appendChild(colContainer2);
-sectContainer.appendChild(rowContainer2);
-rowContainer2.appendChild(colContainer3);
-rowContainer2.appendChild(colContainer4);
-rowContainer2.appendChild(colContainer5);
-
-sectContainer.className ='container'
-rowContainer.className ='row'
-colContainer.className ='col'
-colContainer2.className ='col' 
-rowContainer2.className ='row'
-colContainer3.className ='col1' 
-colContainer4.className ='col1' 
-colContainer5.className ='col1'
-
-console.log(sectContainer);
-
-colContainer.innerText = 'Fizz buzz is a group word game for children to teach them about division. Players take turns to count incrementally, replacing any number divisible by three with the word "fizz", and any number divisible by five with the word "buzz", and any number divisible by both three and five with the word "fizzbuzz".'
-
-
-
-// const htmlCont  = document.getElementById('section1')
-// htmlCont.append(sectContainer)
